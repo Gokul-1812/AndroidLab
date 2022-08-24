1d7ace681a200ab9a73268dd65c45928d2059a53
Gokul-1812 <gokulvc1812@gmail.com>
2022-08-24 11:34:47 +0530

    Program4

---
+++ Implicit_Intent/app/src/main/java/com/example/implicit_intent/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.implicit_intent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mapint = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:9.985017, 76.724802"));
+                startActivity(mapint);
+
+            }
+        });
+    }
+}
